c652c82d1459a5e70f0afde6c0c11a5c62246ef9
Eduardo Martins Peres <eduardomartinsperes@hotmail.com>
2021-07-10 18:33:50 -0300

    aulas de javascript e word de anotações

---
+++ aulascript.js
@@ -0,0 +1,26 @@
+
+var myName = "Professora Simara";
+const pattern ="ECMAScript";
+
+console.log(myName)
+
+var colors = ["loira", "morena", "branquinha", "ruiva"];
+
+for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+var i = 1
+
+while (i <= 10) {
+    console.log(i);
+    i++;
+        
+}
+
+
+
+
+
+
+ 
