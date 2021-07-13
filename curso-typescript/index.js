d7f9c6beeee4451cc39f40376aceed7c38131b8c
Eduardo Martins Peres <eduardomartinsperes@hotmail.com>
2021-07-13 01:08:48 -0300

    pasta do módulo de typescript

---
+++ curso-typescript/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function somar(num1, num2) {
+    return num1 + num2;
+}
+var tapas1 = 10;
+var tapas2 = 5;
+var resultado = somar(tapas1, tapas2);
+console.log('vai tomar ' + resultado + ' tapas');
---
+++ curso-typescript/index.ts
@@ -0,0 +1,9 @@
+function somar(num1: number, num2: number){
+    return num1 + num2
+}
+
+const tapas1 = 10;
+const tapas2 = 5;
+
+const resultado = somar(tapas1, tapas2);
+console.log('vai tomar ' + resultado + ' tapas');
